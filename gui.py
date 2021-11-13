467a26609c761040bf929a97fedfa7e0a9a6dd18
Maxim Kozenko <maxim.kozenko2001@gmail.com>
2021-11-13 19:12:26 +0200

    Added DI pattern - constructor injection for class GUI

--- gui.py
+++ gui.py
@@ -2,29 +2,33 @@ class GUI:
     _user_choose = None
     _menu_text = "Выберите операцию:\n1.Суммирование\n2.Умножножение\n3.Деление\n4.Вычитание\n5.Выход\n:"
 
+    def __init__(self, calculator):
+        """Constructor for base values and realization DI by constructor"""
+        self.calculator = calculator
+
     def show_menu(self):
         '''Отображение меню пользователя'''
         self._user_choose = int(input(self._menu_text))
 
-    def run_command(self, calculator):
+    def run_command(self):
         '''Запуск основных команд для работы с калькулятором'''
         answer = None
         self.show_menu()
         if self._user_choose > 5:
             print("Такого  варианта операции не существует!\nПовторите операцию.")
-            return self.run_command(calculator)
+            return self.run_command(self.calculator)
         else:
             if self._user_choose == 5:
                 exit(0)
             numbers = list(map(lambda x: int(x), self.get_numbers()))
             if self._user_choose == 4:
-                answer = calculator.subtraction(numbers[0], numbers[1])
+                answer = self.calculator.subtraction(numbers[0], numbers[1])
             if self._user_choose == 3:
-                answer = calculator.division(numbers[0], numbers[1])
+                answer = self.calculator.division(numbers[0], numbers[1])
             if self._user_choose == 2:
-                answer = calculator.multiplication(numbers[0], numbers[1])
+                answer = self.calculator.multiplication(numbers[0], numbers[1])
             if self._user_choose == 1:
-                answer = calculator.addition(numbers[0], numbers[1])
+                answer = self.calculator.addition(numbers[0], numbers[1])
             return answer
 
     def get_numbers(self):
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from gui import  GUI
 from calculators import EconomicalCalculator
 calculator = EconomicalCalculator()
-gui_interface = GUI()
+gui_interface = GUI(calculator)
 while True:
-    answer = gui_interface.run_command(calculator)
+    answer = gui_interface.run_command()
     print(f' Результат вычислений: {answer}')

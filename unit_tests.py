7ca43594a9d177612ce1d1d58f303d2d91b25aee
Maxim Kozenko <maxim.kozenko2001@gmail.com>
2021-11-18 09:03:49 +0200

    Updated tests, added mock logic by patch - decorator

--- unit_tests.py
+++ unit_tests.py
@@ -2,29 +2,30 @@
 import sys
 import unittest
 from unittest import TestCase
-from calculators import EconomicalCalculator
+from unittest.mock import patch, Mock
 from math import inf
 
 
 class TestCalculator(TestCase):
     # class for create and run with tests
-    def setUp(self):
-        self.base_calculator = EconomicalCalculator()
 
-    def test_easy_addition(self):
+    @patch(target='calculators.EconomicalCalculator.addition', return_value=164)
+    def test_easy_addition(self, func):
         """Method for test and check addition function with normal numbers"""
-        answer = self.base_calculator.addition(24, 140)
+        answer = func(24, 140)
         self.assertAlmostEqual(answer, 164)
 
-    def test_hard_addition(self):
+    @patch(target='calculators.EconomicalCalculator.addition', return_value=inf)
+    def test_hard_addition(self, func):
         """Method for test and check addition function with max numbers"""
-        answer = self.base_calculator.addition(inf, sys.maxsize)
+        answer = func(inf, sys.maxsize)
         self.assertAlmostEqual(answer, inf)
 
-    def test_division(self):
+    @patch(target='calculators.EconomicalCalculator.division', return_value=2.25)
+    def test_division(self, func):
         """Method for test and check division function with exception"""
-        with self.assertRaises(ZeroDivisionError):
-            self.base_calculator.division(9, 0)
+        answer = func(9, 14)
+        self.assertAlmostEqual(answer, 2.25)
 
 
 if __name__ == "__main__":
